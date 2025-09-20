41901c236186bdfcf45f8b650ba0bf3402896546
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-09-20 13:42:27 -0500

    Create notifier.py

---
+++ notifier.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from twilio.rest import Client
+
+def send_whatsapp(message: str):
+    client = Client(
+        st.secrets["twilio"]["account_sid"],
+        st.secrets["twilio"]["auth_token"]
+    )
+
+    msg = client.messages.create(
+        body=message,
+        from_=f"whatsapp:{st.secrets['twilio']['sandbox_number']}",
+        to=f"whatsapp:{st.secrets['twilio']['to_number']}"
+    )
+    return msg.sid
