db52043899edf171f8b6842bb99cd2a21d52961e
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-09-26 02:38:01 -0500

    Update __init__.py

--- sections/__init__.py
+++ sections/__init__.py
@@ -1,10 +1,1 @@
 # sections/__init__.py
-from .inspector import render_inspector
-from .controls import render_controls
-from .history import render_history
-
-__all__ = [
-    "render_inspector",
-    "render_controls",
-    "render_history",
-]
